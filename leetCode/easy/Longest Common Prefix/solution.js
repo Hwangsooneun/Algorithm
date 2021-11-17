31b77196be00dd3d5515e2e692fb69201d8f3245
Hwangsooneun <catalyst8849@gmail.com>
2021-11-17 15:40:52 +0900

    2021-11-17

---
+++ leetCode/easy/Longest Common Prefix/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+// runtime 80ms
+ var longestCommonPrefix = function(strs) {
+  for (let i = 0; i < strs[0].length; i++) {
+      for (let j = 1; j < strs.length; j++) {
+          if (strs[0][i] !== strs[j][i]) return strs[0].slice(0, i)
+      }
+  }
+  return strs[0]
+};
