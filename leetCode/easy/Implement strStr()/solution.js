1ee15d44ae383d32b7aab8edf694b19545deac46
Hwangsooneun <catalyst8849@gmail.com>
2021-11-29 23:59:19 +0900

    2021-11-29

---
+++ leetCode/easy/Implement strStr()/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+// runtime 64ms
+ var strStr = function(haystack, needle) {
+  return haystack.indexOf(needle)
+};
+/*
+지금까지 1개이상의 문자열의 인덱스찾기가 되는지 몰랐다...
+결과는 됨!
+*/
